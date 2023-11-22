f93ca83a97f003dcc5930877fd5056035daecc96
Geoff Russell <geoffrey.russell@gmail.com>
2023-11-22 14:32:28 +1030

    Form complete data series of cum solar pv installed

--- SolarWaste/app.R
+++ SolarWaste/app.R
@@ -9,8 +9,17 @@
 
 library(shiny)
 library(tidyverse)
+# This global solar capacity factor was chosen so that the TWh for 2021 in the World Energy Stats (ex-BP) matches
+# the GW in the IEA Net Zero plan
+solarCF<-0.13
 # Data
 # https://iea.blob.core.windows.net/assets/9a698da4-4002-4e53-8ef3-631d8971bf84/NetZeroRoadmap_AGlobalPathwaytoKeepthe1.5CGoalinReach-2023Update.pdf
+# Now we get the World Energy Stats TWh from the 2023 edition and use the capacity factor (solarCF) to estimate
+# the installed GW for that year
+solarTWh<-read_csv("solartwh.csv") 
+solarGW<-solarTWh %>% mutate(Year=ymd(paste0(Year,"-01-01")),cumGWinstalled=TWh*1e12/(solarCF*24*365)/1e9) %>%
+  select(cumGWinstalled,Year)
+print(solarGW)
 thisYear<-2022
 years<-28
 pvGWThisYear<-1145
@@ -47,7 +56,6 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins", "Number of bins:", min = 1, max = 50, value = 30),
             sliderInput("pvGrowthRate1","Annual PV growth rate  to 2030 (%)",min = 1, max = 30, value = 23),
             sliderInput("pvGrowthRate2","Annual PV growth rate 2030 to 50 (%)",min = 1, max = 30, value = 12),
             sliderInput("pvTonnagePerGW","Tonnage per GW of PV panels ('000 tonnes)",min = 30, max = 150, value = 70)
@@ -68,6 +76,10 @@ have two values, one from now to 2030 and the second rate from 2030 to 2050. Thi
 is much easier to grow by some percentage when you are small than when you are large; the bottleneck typically being
 the flow of materials.
 
+### IEA Net Zero target 
+
+The IEA Net Zero by 2050 target is for 18,750 GW ([as updated in 2023](https://iea.blob.core.windows.net/assets/9a698da4-4002-4e53-8ef3-631d8971bf84/NetZeroRoadmap_AGlobalPathwaytoKeepthe1.5CGoalinReach-2023Update.pdf)).
+
                     "),
            plotOutput("distPlot")
         )
@@ -76,21 +88,20 @@ the flow of materials.
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
     output$distPlot <- renderPlot({
       pvProdto2030<-makeExpProduction(input$pvGrowthRate1,pvGWThisYear,8)
       df1<-tibble(
-        produced=(1:8 %>% map_dbl(pvProdto2030)),
-        year=seq(ymd('2023-01-01'),ymd('2030-01-01'),by='1 year')
+        cumGWinstalled=(1:8 %>% map_dbl(pvProdto2030)),
+        Year=seq(ymd('2023-01-01'),ymd('2030-01-01'),by='1 year')
       )
-      pvProdto2050<-makeExpProduction(input$pvGrowthRate2,df1$produced[8],19)
+      pvProdto2050<-makeExpProduction(input$pvGrowthRate2,df1$cumGWinstalled[8],19)
       df2<-tibble(
-        produced=(1:20 %>% map_dbl(pvProdto2050)),
-        year=seq(ymd('2031-01-01'),ymd('2050-01-01'),by='1 year')
+        cumGWinstalled=(1:20 %>% map_dbl(pvProdto2050)),
+        Year=seq(ymd('2031-01-01'),ymd('2050-01-01'),by='1 year')
       )
-      df<-bind_rows(df1,df2)
+      df<-bind_rows(solarGW,df1,df2)
       print(df,n=30)
-      df %>% ggplot(aes(x=year,y=produced))+geom_col()
+      df %>% ggplot(aes(x=Year,y=cumGWinstalled))+geom_col()
     })
 }
 
