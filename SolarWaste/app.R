191ba6424ba196294417ac153528710de435c248
Geoff Russell <geoffrey.russell@gmail.com>
2023-11-23 17:30:10 +1030

    refactor and polish

--- SolarWaste/app.R
+++ SolarWaste/app.R
@@ -54,7 +54,115 @@ nyears<-lastYear-firstYear+1
 bFailed<-rep(0,nyears)
 bRecycled<-rep(0,nyears)
 bRecycledGWh<-rep(0,nyears)
-genWasteData<-function(input) {
+
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Solar Waste Tonnage over time"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("pvGrowthRate1","Growth rate to 2030 (%)",min = 1, max = 30, value = 23),
+            sliderInput("pvGrowthRate2","Growth rate 2030 to 2050 (%)",min = 1, max = 30, value = 11),
+            sliderInput("pvTonnagePerGW","Panel tonnage per GW ('000 tonnes)",min = 30, max = 150, value = 70),
+            sliderInput("pvLifeSpan","Average lifespan (years))",min = 15, max = 50, value = 30),
+            sliderInput("pvRecycling22","Recycling Capacity 2022 (GW)",min = 10, max = 50, value = 10),
+            sliderInput("pvRecyclingCAGR","Recycling CAGR (%)",min = 5, max = 20, value = 5),
+            dateInput("pvStartYear","Pick start date",
+                           value=ymd("2010-01-01"),
+                           min=ymd("2010-01-01"),max=ymd("2050-01-01"),
+                           format="yyyy-mm-dd"),
+            markdown("Geoff Russell, Alpha Testing, V0.4 November 2023")
+        ),
+
+        # Show a plot of the generated distribution
+        
+        mainPanel(
+           markdown("## Gigawatts of solar PV panels"),
+           plotOutput("distPlot"),
+           uiOutput("notes"),
+           markdown("## Material required"),
+           plotOutput("tonnagePlot"),
+markdown("
+
+## Model assumptions 
+
+The graph above, with its initial slider settings, 
+models the global rollout of solar PV panels according to the IEA Net Zero by 2050.
+This IEA plan proposes some 18,750 GW of solar PV ([as updated in 2023](https://iea.blob.core.windows.net/assets/9a698da4-4002-4e53-8ef3-631d8971bf84/NetZeroRoadmap_AGlobalPathwaytoKeepthe1.5CGoalinReach-2023Update.pdf)).
+As you can see, the model illustrated in the graph above, yields an operational level of 17,440 GW by 2050. The 
+small difference may be down to some minor variations in the PV panel failure mechanism I've used, or the
+initial conditions; the number of GW of solar rolled out between 2012 and 2022. 
+I use the World Energy Statistics data for historical PV capacity; which may not be what the 
+IEA use.
+
+The initial settings mirror the assumptions in the IEA netzero by 2050 plan
+and envisage compound annual growth rates (CAGR) of 23% out to 2030 and 11% between 2030 and
+2050. These two numbers determine the growth in panel production. 
+
+The IEA don't discuss PV module failure in the NZ2050 report, unless I missed it. But one is implied by
+their numbers. Those CAGRs without any panel failure would result in a much bigger 
+installed gigawatt (GW) base by 2050.
+
+**PV panel failure**
+
+The failure model merely takes panels out of the installed base according to a fairly standard industrial
+model based on the mean time to failure of panels (ie. average panel lifespan).
+
+The initial value of this lifespan parameter is 30 years. 
+
+**PV recycling**
+
+Recycling is modelled by magic whereby failed panels are simply added back to the
+operational base until the recycling capacity for the year is reached. We allow the recycling capacity and its growth
+rate to be changed; like all the other model parameters.
+
+A more realistic model would dial back the production growth rate as the recycling rate increased. This would keep
+the installed base tracking the IEA planning target.
+
+I don't really have any idea how much PV recycling is being done at present. I see media report of small or very small
+projects. So my guess at 10 GW per year would amount to about 30 million panels a year. 
+
+**Panel weight and waste**
+
+The initial setting of 70,000 tonnes of mined materials per GW or 
+PV panels is based on reading a stack of specification sheets. Panels used in solar farms are typically heavier than
+those used on rooftops.
+
+No account is made of the aluminium, steel and possibly concrete
+used for mounting panels, either on rooftops or in utility scale farms. The weight of mounting materials often exceeds that of
+the panels themselves. For utility scale farms, the soil and weather will determine if concrete footings are 
+required; the tendency seems, for obvious reasons, to try and avoid that expense.
+
+### Guide to the graph 
+
+All the bars on the chart are in gigawatts (GW). The calculated tonnage of waste below the graph is just estimated
+by multiplying the GW by the tonnage per factor figure in the slider.
+
+The bars are as follows:
+
+1. cumFailed ... these are panels which have failed but haven't been recycled because of a lack of capacity 
+1. cumInstalled ... total GW included failed panels  
+1. operational ... cuminstalled - cumfailed + recycled
+1. produced ... new production from freshly mined materials 
+1. recycled ... production from recycled material; assumed to be perfect 
+
+
+
+
+
+                    ")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    genWasteData<-reactive({
       recycleFun<-makeRecycle(input$pvRecyclingCAGR,input$pvRecycling22)
       failFun<-makeFail(input$pvLifeSpan)
       pvProdto2030<-makeExpProduction(input$pvGrowthRate1,pvGWThisYear,8)
@@ -113,95 +221,31 @@ genWasteData<-function(input) {
       
       
       df2
-}
-
-
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Solar Waste Tonnage over time"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("pvGrowthRate1","Growth rate to 2030 (%)",min = 1, max = 30, value = 23),
-            sliderInput("pvGrowthRate2","Growth rate 2030 to 2050 (%)",min = 1, max = 30, value = 11),
-            sliderInput("pvTonnagePerGW","Panel tonnage per GW ('000 tonnes)",min = 30, max = 150, value = 70),
-            sliderInput("pvLifeSpan","Average lifespan (years))",min = 15, max = 50, value = 30),
-            sliderInput("pvRecycling22","Recycling Capacity 2022 (GW)",min = 10, max = 50, value = 10),
-            sliderInput("pvRecyclingCAGR","Recycling CAGR (%)",min = 5, max = 20, value = 5)
-        ),
-
-        # Show a plot of the generated distribution
-        
-        mainPanel(
-           plotOutput("distPlot"),
-           uiOutput("notes"),
-markdown("
-
-### Model assumptions 
-
-The graph below models the global rollout, failure, recycling and waste associated with solar PV panels.
-
-The initial settings mirror the assumptions in the IEA netzero by 2050 plan.
-They envisage compound annual growth rates (CAGR) of 23% to 2030 and 11% between 2030 and
-2050. They don't, as far as I know, specify a failure model for the panels, but one is implied by
-their numbers. I've used an average panel lifespan of 30 years and a fairly standard failure model. When
-this failure model is added to the growth model, then the operational GW of solar in 2050 is a reasonable
-match to the IEA target.
-
-We add magic recycling to the model whereby panels which fail are simply added back to the
-installation stream until the recycling capacity is reached. We allow the recycling capacity and its growth
-rate to be explored; like all the other model parameters.
-
-### IEA Net Zero target 
-
-The IEA Net Zero by 2050 target for solar PV is 18,750 GW ([as updated in 2023](https://iea.blob.core.windows.net/assets/9a698da4-4002-4e53-8ef3-631d8971bf84/NetZeroRoadmap_AGlobalPathwaytoKeepthe1.5CGoalinReach-2023Update.pdf)).
-The initial settings on this model yield 17,440 GW. The difference may be down to some minor difference in the failure model or the
-initial conditions. I use the World Energy Statistics data for historical PV capacity; which may not be what the 
-IEA use.
-
-### Guide to the graph 
-
-PV panels are assumed to be produced and installed according to two rates; the 
-rate from now to 2030 and the rate from 2030 to 2050. They have an average lifespan which determines the
-failure rate using a exponential failure model (one of a number of standard failure models). Any failed panel
-can be recycled upto the limit of the recycling capacity which is determined by an estimate of the capacity
-in 2022 and a specified growth rate. 
-
-So the chart gives various numbers.
-
-1. cumfailed ... these are panels which have failed but can't be recycled because of a lack of capacity 
-1. cuminstalled ... total GW included failed panels  
-1. operational ... cuminstalled - cumfailed + recycled
-1. produced ... new production from freshly mined materials 
-1. recycled ... production from recycled material; assumed to be perfect 
-
-                    ")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+    })
     output$notes <- renderUI({
-      df<-genWasteData(input)
+      df<-genWasteData()
       op2050<-df %>% filter(State=="operational") %>% summarise(mx=max(GW))
       waste2050<-df %>% filter(State=="cumFailed") %>% summarise(mx=max(GW))
       markdown(paste0(
-        "GW of operational solar in 2050: **",comma(op2050),"**\n\n",
-        "Accumulated waste by 2050: **",comma(waste2050*1000*input$pvTonnagePerGW/1e6),"** million tonnes\n"
+        "Operational PV panels in 2050: **",comma(op2050),"** GW\n\n",
+        "Accumulated PV panel waste by 2050: **",comma(waste2050*1000*input$pvTonnagePerGW/1e6),"** million tonnes\n"
         ))
     })
     output$distPlot <- renderPlot({
-      df<-genWasteData(input)
+      df<-genWasteData()
       mx<-df %>% filter(State=="operational") %>% summarise(mx=max(GW))
       print(mx)
+      y1<-ymd(as.character(input$pvStartYear))
+      print(y1)
       df %>% ggplot(aes(x=Year,y=GW,fill=State))+
-        geom_col(position="dodge")+labs(y="Gigawatts") +
-        annotate('text',x=ymd("2030-01-01"),y=30000,label=paste0("Operational GW of PV in 2050:",comma(mx)))
+        geom_col(position="dodge")+labs(y="Gigawatts") + xlim(y1,ymd("2050-10-10")) +
+        annotate('text',x=ymd("2030-01-01"),y=40000,vjust=0,label=paste0("Operational PV in 2050: ",comma(mx),"GW"),size=5)
+    })
+    output$tonnagePlot <- renderPlot({
+      df<-genWasteData()
+      y1<-ymd(as.character(input$pvStartYear))
+      df %>% ggplot(aes(x=Year,y=GW*input$pvTonnagePerGW*1000/1e6,fill=State))+ xlim(y1,ymd("2050-10-10")) +
+        geom_col(position="dodge")+labs(y="million tonnes") 
     })
 }
 
